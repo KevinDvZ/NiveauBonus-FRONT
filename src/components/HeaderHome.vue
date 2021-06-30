1c2d408e174954d797fae1f363f6f5c444126ea9
Kevin DvZ <kevin.deveza@gmail.com>
2021-06-30 16:21:00 +0200

    init menu home

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="global-header__navigation">
+      <ul class="global-toolbar">
+        <li v-for="(bouton,index) in boutonsMenu"
+            v-bind:key="index"
+        >
+          <button>
+            <span>{{bouton.intitule}}</span>
+            <span></span>
+          </button>
+          <span>{{bouton.intitule}}</span>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: "MenuHome",
+  computed : {
+    boutonsMenu(){
+      return this.$store.getters.boutonsMenu;
+    }
+  }
+}
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+.global-header__navigation {
+  background-color: $color-background-menu;
+
+}
+
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,37 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    boutonsMenu: [
+      {
+        ordre:1,
+        intitule:"Qui-suis je ?",
+        logo: "@/assets/boutons/kevin.png"
+      },
+      {
+        ordre:2,
+        intitule:"Créations",
+        logo: "@/assets/boutons/webDesignBrush.svg"
+      },
+      {
+        ordre:3,
+        intitule:"Blog",
+        logo: "@/assets/boutons/webDesignBrush.svg"
+      },
+      {
+        ordre:4,
+        intitule:"Labo & Bazar",
+        logo: "@/assets/boutons/webDesignBrush.svg"
+      },
+    ]
+
+
   },
+  getters: {
+    boutonsMenu(state) {
+      return state.boutonsMenu;
+    }
+  },
+
   mutations: {
   },
   actions: {
